805e4a47b8aca07a5a7fa1334006d5d781ee3fb1
Cenere Aereo <aeroappstore@gmail.com>
2022-04-28 10:24:16 +0100

    SP-1 Updated functionality to meet brief and added comments

--- src/main/scala/BonusCalc.scala
+++ src/main/scala/BonusCalc.scala
@@ -1,12 +1,17 @@
-import BonusCalc.{bonus, salary}
+/* Easy - Do I get a Bonus?
+Write a function that takes two arguments: salary (`Int`) and bonus (`Boolean`).
+If bonus is `true`, return the salary plus 20%, rounded down. Otherwise, return the salary.
+You should return the answer as a string, prefixed with `£`.
+
+Examples
+- Given 100 and true, return "£120"
+- Given 100 nd false, return "100"
+- Given 99 and true, return "£118"
+*/
 
 import scala.io.StdIn
 
 object BonusCalc extends App {
-  println("What is your salary? ")
-  val salary: Int = StdIn.readInt()
-  println("Bonus - true or false? ")
-  val bonus: Boolean = StdIn.readBoolean()
 
   def calculateBonus(salary: Int, bonus: Boolean): Unit = {
     val salaryFloat = salary.toFloat
@@ -21,6 +26,7 @@ object BonusCalc extends App {
     }
   }
 
-  BonusCalc.calculateBonus(salary, bonus)
-  // DoIGetABonus.calculateBonus("200", "false")
+  BonusCalc.calculateBonus(100, true)
+  BonusCalc.calculateBonus(100, false)
+  BonusCalc.calculateBonus(99, true)
 }
